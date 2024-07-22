cd115ba297ddcf47f39a5c3f7fb3351fce716b28
Dmitrii Kotov <dmitrii.kotov@jetbrains.com>
2024-07-22 13:36:11 +0300

    DuplicateByteCodeFinder script corrected
    Signed-off-by: Dmitrii Kotov <dmitrii.kotov@jetbrains.com>

--- scripts/DuplicateByteCodeFinder.py
+++ scripts/DuplicateByteCodeFinder.py
@@ -8,7 +8,6 @@ dataset_path = '.'  # Path to the dataset
 project_path = '.'  # Path to the project that will be compiled
 compilation_failed_file = 'compilationFailed.txt'
 duplicates_file = 'duplicates.txt'
-compilation_successful_file = 'compilationSuccessful.txt'
 
 directories = [os.path.join(dataset_path, d) for d in os.listdir(dataset_path) if
                os.path.isdir(os.path.join(dataset_path, d))]
@@ -25,12 +24,9 @@ def copy_file_to_package(src_file, package):
 
 def compile_project():
     env = os.environ.copy()
-    try:
-        result = subprocess.run(['./gradlew', 'clean', 'build', '--info'], cwd=project_path, capture_output=True,
-                                text=True, timeout=10, env=env, shell=True)
-        return result.returncode == 0, result.stderr
-    except subprocess.TimeoutExpired:
-        return False, "Build command timed out"
+    result = subprocess.run(['./gradlew', 'clean', 'build'], cwd=project_path, capture_output=True, text=True, env=env,
+                            shell=True)
+    return result.returncode == 0, result.stderr
 
 
 def extract_bytecode(class_file):
@@ -43,6 +39,19 @@ def get_file_base_name(file_name):
     return file_name.split("-", 1)[0]
 
 
+def package_from_content(content):
+    if "package day1" in content:
+        return 'day1'
+    elif "package day2" in content:
+        return 'day2'
+    elif "package day3" in content:
+        return 'day3'
+    elif "package day4" in content:
+        return 'day4'
+    else:
+        return 'singleTasks'
+
+
 def process_files(directory):
     start_time = time.time()
     for root, _, files in os.walk(directory):
@@ -53,34 +62,20 @@ def process_files(directory):
                 file_path = os.path.join(root, file)
                 with open(file_path, 'r') as f:
                     content = f.read()
-                    if "package day1" in content:
-                        package = 'day1'
-                    elif "package day2" in content:
-                        package = 'day2'
-                    elif "package day3" in content:
-                        package = 'day3'
-                    elif "package day4" in content:
-                        package = 'day4'
-                    else:
-                        package = 'singleTasks'
-
+                    package = package_from_content(content)
                 dest_dir = os.path.join(project_path, 'src', package)
                 os.makedirs(dest_dir, exist_ok=True)
                 dest_file = os.path.join(dest_dir, new_file_name)
 
                 shutil.copy(file_path, dest_file)
                 compiled, error = compile_project()
-                if compiled:
-                    with open(compilation_successful_file, 'a') as csf:
-                        csf.write(f'{file_path}\n')
-                else:
+                if not compiled:
                     with open(compilation_failed_file, 'a') as cf:
                         cf.write(f'{file_path}: {error}\n')
                     os.remove(dest_file)
                     continue
 
-                class_file = dest_file.replace('.kt', '.class').replace('./src/singleTasks',
-                                                                        './build/classes/kotlin/main')
+                class_file = dest_file.replace('.kt', '.class').replace('./src/', './build/classes/kotlin/main/')
                 if os.path.exists(class_file):
                     bytecode = extract_bytecode(class_file)
                     if bytecode in bytecode_map:
